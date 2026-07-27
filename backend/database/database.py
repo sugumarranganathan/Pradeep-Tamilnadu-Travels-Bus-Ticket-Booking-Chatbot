47533c639360344106778de426a60967c0186d0b
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 16:48:39 +0530

    Update database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,1 +1,26 @@
-s
+"""
+Database Connection
+Version : 1.0.0
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config.settings import settings
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
