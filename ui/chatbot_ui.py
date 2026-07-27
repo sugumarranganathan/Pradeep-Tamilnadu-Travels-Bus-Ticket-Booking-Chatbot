b17a5fb507b1c3f98ac6e454821950fe11463434
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 17:16:13 +0530

    Update chatbot_ui.py

--- ui/chatbot_ui.py
+++ ui/chatbot_ui.py
@@ -1,1 +1,17 @@
-s
+from conversation.conversation_manager import ConversationManager
+
+manager = ConversationManager()
+
+
+def chatbot_reply(message, history):
+
+    if history is None:
+        history = []
+
+    reply = manager.reply(message)
+
+    history.append(
+        (message, reply)
+    )
+
+    return "", history
