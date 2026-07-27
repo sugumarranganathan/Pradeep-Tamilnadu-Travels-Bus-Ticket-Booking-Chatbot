018526db8b016c475b8610da18eb394f16213703
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 16:58:10 +0530

    Update rag_engine.py

--- backend/rag/rag_engine.py
+++ backend/rag/rag_engine.py
@@ -1,1 +1,30 @@
-s
+"""
+Simple RAG Engine
+Version 1
+"""
+
+from rag.knowledge_base import KNOWLEDGE_BASE
+
+
+class RAGEngine:
+
+    def search(self, query):
+
+        query = query.lower()
+
+        if "route" in query:
+            return KNOWLEDGE_BASE["routes"]
+
+        elif "time" in query:
+            return KNOWLEDGE_BASE["timings"]
+
+        elif "fare" in query:
+            return KNOWLEDGE_BASE["fare"]
+
+        elif "cancel" in query:
+            return KNOWLEDGE_BASE["cancellation"]
+
+        elif "support" in query:
+            return KNOWLEDGE_BASE["support"]
+
+        return KNOWLEDGE_BASE["welcome"]
