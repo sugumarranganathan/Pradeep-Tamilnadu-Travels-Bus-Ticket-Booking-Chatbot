a12e5e1ea390708d45819010cf3a41fecbd8c5f8
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 16:50:30 +0530

    Update ticket_schema.py

--- backend/schemas/ticket_schema.py
+++ backend/schemas/ticket_schema.py
@@ -1,1 +1,11 @@
-s
+from pydantic import BaseModel
+
+
+class TicketResponse(BaseModel):
+
+    ticket_number: str
+    passenger_name: str
+    route: str
+    bus_number: str
+    seat_number: str
+    fare: int
