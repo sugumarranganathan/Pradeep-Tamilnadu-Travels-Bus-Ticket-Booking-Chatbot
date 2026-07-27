0769acada26a8072964e5a51df74578ad72dd3e9
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 17:25:09 +0530

    Create test_data_service.py

---
+++ backend/test_data_service.py
@@ -0,0 +1,8 @@
+from services.data_service import DataService
+
+service = DataService()
+
+print(service.get_routes())
+print(service.get_buses())
+print(service.get_timings())
+print(service.get_fares())
