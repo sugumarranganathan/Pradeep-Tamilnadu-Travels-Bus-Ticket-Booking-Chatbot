a5cb48d494ad35778b239b2cc8fd9f73c9705ac7
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 16:54:57 +0530

    Update booking_agent.py

--- backend/agents/booking_agent.py
+++ backend/agents/booking_agent.py
@@ -1,1 +1,20 @@
-s
+"""
+Booking Agent
+"""
+
+from services.booking_service import get_routes
+
+
+class BookingAgent:
+
+    def start_booking(self):
+
+        return {
+
+            "step": "route_selection",
+
+            "message": "Select Your Route",
+
+            "routes": get_routes()
+
+        }
