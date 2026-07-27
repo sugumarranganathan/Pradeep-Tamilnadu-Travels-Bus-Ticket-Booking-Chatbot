67fb6e84808e770fe1ec3364ad05f994577b272d
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 17:10:56 +0530

    Update pdf_generator.py

--- backend/ticket/pdf_generator.py
+++ backend/ticket/pdf_generator.py
@@ -1,1 +1,17 @@
-s
+"""
+PDF Generator
+Version 1
+"""
+
+
+class PDFGenerator:
+
+    def generate(self, ticket):
+
+        return {
+
+            "pdf_name": f'{ticket["ticket_number"]}.pdf',
+
+            "status": "Generated"
+
+        }
