c850d63f9c36b3379fab735611a984f656ae7081
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 19:54:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ Pradeep Tamilnadu Travels Bus Ticket Booking Chatbot
 
 from ui.app import demo
 
-
 if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
-        server_port=7860
+        server_port=7860,
+        share=True,
+        debug=True
     )
