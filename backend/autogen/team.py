cee15a9419569218aed9806235734b71bc4610e6
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 19:37:32 +0530

    Update team.py

--- backend/autogen/team.py
+++ backend/autogen/team.py
@@ -2,19 +2,15 @@
 Agent Team
 """
 
-from autogen.agents import (
-    RouteAgent,
-    BookingAgent,
-    TicketAgent,
-    SupportAgent
-)
+from backend.agents.booking_agent import BookingAgent
+from backend.agents.ticket_agent import TicketAgent
+from backend.agents.support_agent import SupportAgent
+from backend.agents.supervisor_agent import SupervisorAgent
 
 
 class AgentTeam:
-
     def __init__(self):
-
-        self.route = RouteAgent()
         self.booking = BookingAgent()
         self.ticket = TicketAgent()
         self.support = SupportAgent()
+        self.supervisor = SupervisorAgent()
