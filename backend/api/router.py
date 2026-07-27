d071a11154d690c2d4a50f0b65c69130fe9b0068
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 19:20:12 +0530

    Update router.py

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from api.health import router as health_router
-from api.chatbot import router as chatbot_router
-from api.booking import router as booking_router
-from api.ticket import router as ticket_router
+from backend.api.health import router as health_router
+from backend.api.chatbot import router as chatbot_router
+from backend.api.booking import router as booking_router
+from backend.api.ticket import router as ticket_router
 
 api_router = APIRouter(prefix="/api/v1")
 
