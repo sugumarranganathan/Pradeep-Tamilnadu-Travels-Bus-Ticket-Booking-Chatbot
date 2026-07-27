78a1bc5ef8aef1dfeefb216a83b80a290ee14e56
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 16:39:40 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,10 @@
-s
+from fastapi import FastAPI
+from api.router import api_router
+
+app = FastAPI(
+    title="Pradeep Tamilnadu Travels Bus Ticket Booking Chatbot",
+    description="AI-Powered Multi-Agent Bus Ticket Booking Chatbot using Groq, AutoGen & RAG",
+    version="1.0.0"
+)
+
+app.include_router(api_router)
