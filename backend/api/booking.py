73b082983efdebb28cb83aa60512004700dc9c9f
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-07-27 16:45:31 +0530

    Update booking.py

--- backend/api/booking.py
+++ backend/api/booking.py
@@ -1,1 +1,16 @@
-s
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/booking/menu")
+def booking_menu():
+
+    return {
+        "title": "Bus Ticket Booking",
+        "options": [
+            "1. Chennai → Madurai",
+            "2. Chennai → Coimbatore",
+            "3. Chennai → Trichy",
+            "4. Chennai → Salem"
+        ]
+    }
